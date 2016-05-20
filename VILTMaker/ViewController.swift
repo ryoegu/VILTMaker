edc4e8e59c95090d11963f1d88302dc53c132047
Ryo Eguchi <eguchi@lifeistech.co.jp>
2016-05-20 17:44:44 +0900

    Initial Commit

---
+++ VILTMaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VILTMaker
+//
+//  Created by Ryo Eguchi on 2016/05/20.
+//  Copyright © 2016年 Ryo Eguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
